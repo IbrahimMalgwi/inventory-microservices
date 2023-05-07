004e9040de360100b083823aa5aa4026a5628ec1
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-05-07 20:19:10 +0100

    Implemented webClient

--- inventory-service/src/main/java/com/ganzymalgwi/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/ganzymalgwi/inventoryservice/repository/InventoryRepository.java
@@ -3,10 +3,10 @@ package com.ganzymalgwi.inventoryservice.repository;
 import com.ganzymalgwi.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    default Optional<Inventory> findBySkuCode() {
-        return Optional.empty();
-    }
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCOde);
 }

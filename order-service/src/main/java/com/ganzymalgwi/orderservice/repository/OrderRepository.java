9e7e3c6c5c8149a3213e4ebb024eef968658f40f
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-05-09 03:48:49 +0100

    Refactored, Implemented more features

--- order-service/src/main/java/com/ganzymalgwi/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ganzymalgwi.orderservice.repository;
-
-import com.ganzymalgwi.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- product-service/src/main/java/com/ganzymalgwi/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ganzymalgwi.productservice.repository;
-
-import com.ganzymalgwi.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
